e274575f89765437f5f70f5c6c182a48d22b72d9
Robin Wellner <gyvox.public@gmail.com>
2010-02-23 23:57:10 +0100

    Added vector preview

---
+++ preview/conf.lua
@@ -0,0 +1,10 @@
+function love.conf(t)
+	t.title = "Vector preview"
+	t.modules.joystick = false
+	t.modules.audio = false
+	t.modules.keyboard = false
+	t.modules.image = false
+	t.modules.mouse = false
+	t.modules.sound = false
+	t.modules.physics = false
+end
