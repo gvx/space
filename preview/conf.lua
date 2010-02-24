5c6ed8f8a5f145b101c4c5188229170821595bba
Robin Wellner <gyvox.public@gmail.com>
2010-02-24 09:42:33 +0100

    More preview stuff

--- preview/conf.lua
+++ preview/conf.lua
@@ -2,7 +2,7 @@ function love.conf(t)
 	t.title = "Vector preview"
 	t.modules.joystick = false
 	t.modules.audio = false
-	t.modules.keyboard = false
+	--t.modules.keyboard = false
 	t.modules.image = false
 	t.modules.mouse = false
 	t.modules.sound = false
