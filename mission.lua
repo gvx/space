fcc40ff3d7e443c390f8be48fd518b723cb3e0e8
Robin Wellner <gyvox.public@gmail.com>
2010-06-23 15:06:27 +0200

    Brought a bit of life in mission screen

--- mission.lua
+++ mission.lua
@@ -6,6 +6,7 @@ function mission.load()
 	mission.hadfirstmission = false
 	mission.newmission = nil
 	mission.mission = nil
+	mission.animx = 0
 	mission.list = {}
 	for i,file in ipairs(love.filesystem.enumerate('missions')) do
 		if file:sub(-4) == '.lua' then
@@ -32,12 +33,20 @@ function mission.update(dt)
 end
 
 function mission.missionupdate(dt)
+	if not mission.finishedanim then
+		mission.animx = mission.animx + dt
+		if mission.animx > .5 then
+			mission.finishedanim = true
+		end
+	end
 end
 
 function mission.missiondraw()
 	love.graphics.setColor(255,255,255)
 	love.graphics.printf(mission.newmission.description, 40, 50, 720)
 	love.graphics.print(mission.newmission.canrefuse and 'Press Enter to accept or Escape to refuse.' or 'Press Enter or Escape to accept.', 40, 560)
+	love.graphics.setColor(255,255,255,mission.animx*100)
+	love.graphics.rectangle('fill', 0, 535, 800, 40)
 end
 
 function mission.mission_debriefdraw()
@@ -57,6 +66,8 @@ function states.mission.keypressed.escape()
 		mission.newmission.refuse()
 	end
 	mission.newmission = nil
+	mission.animx = 0
+	mission.finishedanim = nil
 	love.graphics.setFont(smallfont)
 	state.current = 'game'
 end
@@ -64,6 +75,8 @@ end
 function states.mission.keypressed.enter()
 	mission.mission = mission.newmission
 	mission.newmission = nil
+	mission.animx = 0
+	mission.finishedanim = nil
 	love.graphics.setFont(smallfont)
 	state.current = 'game'
 	if mission.mission.accept then
