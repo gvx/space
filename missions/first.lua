f724b36b686f93ef6829ab3947c9decd1ff0c927
Robin Wellner <gyvox.public@gmail.com>
2010-06-28 10:58:47 +0200

    Grammatical fixes in first mission (thanks bartbes)

--- missions/first.lua
+++ missions/first.lua
@@ -17,7 +17,7 @@ I want to thank you for delivering that cargo.
 
 If you ever want to work for SpaceCorp again, just land on a planet where SpaceCorp has a division (which is nearly any planet you can find) and take on a mission there.
 
-Of course other parties have missions for you to pick from as well, but they won't pay as much as we.
+Of course other parties have missions for you to pick from as well, but they won't pay as much as we do.
 
 Have fun.]],
 	canrefuse=false,
