6d95fa7d40aa46aac474e5a42654ac1d12839b6c
Robin Wellner <gyvox.public@gmail.com>
2010-07-02 17:37:14 +0200

    Hopefully makes hooks work

--- hook.lua
+++ hook.lua
@@ -1,16 +1,15 @@
 -- based on thelinx's Public Domain UaLove hook system
-hook = {}
+hook = {hooks = {}}
 
 function hook.add(event, func, id)
-	hook.hooks[id] = {event, func}
+	hook.hooks[event] = hook.hooks[event] or {}
+	table.insert(hook.hooks[event], func)
 end
 
 function hook.call(event, ...)
-	for k,v in pairs(hook.hooks) do
-		if v[1] == event then
-			if v[2](...) then
-				break
-			end
+	for i=1,#hook.hooks[event] do
+		if hook.hooks[event][i](...) then
+			break
 		end
 	end
 end
