91249d2608c41dae15d6c291d714e072bcae79ad
Robin Wellner <gyvox.public@gmail.com>
2010-07-28 17:07:17 +0200

    The journey for Game Saving and Loading continues

---
+++ file.lua
@@ -0,0 +1,31 @@
+local modules = {map=map, graphics=graphics,
+	ui=ui, player=player, ai=ai, ships=ships,
+	physics=physics, mainmenu=mainmenu,
+	base=base, mission=mission,
+	diplomacy=diplomacy, help=help, conv=conv}
+
+function savegame(filename)
+	local buildsave = {}
+	for key, mod in pairs(modules) do
+		buildsave[key] = {}
+		for k, v in pairs(mod) do
+			if mod.allowsave(k) then
+				buildsave[key][k] = v
+			end
+		end
+	end
+	-- now write buildsave to filename
+end
+
+function loadgame(filename)
+	local ok, chunk = pcall(love.filesystem.load, filename)
+	if ok then
+		for key, mod in pairs(chunk()) do
+			for k, v in pairs(mod) do
+				modules[key][k] = v
+			end
+		end
+	else
+		-- display a distressing message?
+	end
+end
